e28d86d7592afe4d54bb8fad14f7689ce2c76498
Karanjha <karankr088@gmail.com>
2025-12-04 22:49:40 +0530

    trying to make it work

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -8,7 +8,7 @@ from pwdlib import PasswordHash
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key-for-dev")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
